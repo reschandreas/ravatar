6aeb5a1488e6ad5f550e4d05ae66a65b6846d98f
Andreas Resch <andreas@resch.io>
2024-04-01 19:15:50 +0200

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 mod config;
 
-use std::{env, fs};
 use std::path::{Path, PathBuf};
 use std::thread;
 use std::time::Instant;
+use std::{env, fs};
 
-use actix_web::{App, get, HttpResponse, HttpServer, web};
+use crate::config::Config;
 use actix_web::http::StatusCode;
+use actix_web::middleware::Logger;
 use actix_web::web::Query;
+use actix_web::{get, web, App, HttpResponse, HttpServer};
 use filetime::FileTime;
 use futures::{
     channel::mpsc::{channel, Receiver},
@@ -15,12 +17,12 @@ use futures::{
 };
 use image::io::Reader as ImageReader;
 use md5::Md5;
-use notify::{Event, RecommendedWatcher, RecursiveMode, Watcher};
 use notify::event::DataChange::Content;
-use notify::event::ModifyKind;
-use sha2::{Digest, Sha256};
+use notify::event::{ModifyKind, RenameMode};
+use notify::{Event, RecommendedWatcher, RecursiveMode, Watcher};
+use rayon::prelude::*;
 use serde::Deserialize;
-use crate::config::Config;
+use sha2::{Digest, Sha256};
 
 #[derive(Clone)]
 struct AppState {
@@ -48,26 +50,30 @@ async fn main() -> std::io::Result<()> {
     let cloned_config = config.clone();
     thread::spawn(move || {
         println!("starting resizing");
-        let raw_path = cloned_config.raw.clone();
+        let binding = cloned_config.raw.clone();
+        let raw_path = Path::new(&binding);
         resize_default(cloned_config.clone());
-        cache_directory(raw_path.clone(), cloned_config.clone());
+        process_directory(raw_path, cloned_config.clone());
     });
     let state = AppState {
         config: config.clone(),
     };
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
     HttpServer::new(move || {
-        App::new().service(web::scope(&prefix)
-            .app_data(web::Data::new(state.clone()))
-            .service(avatar)
+        App::new().wrap(Logger::default()).service(
+            web::scope(&prefix)
+                .app_data(web::Data::new(state.clone()))
+                .service(avatar)
+                .service(hash),
         )
     })
-        .bind(("127.0.0.1", port))?
-        .run()
-        .await
+    .bind(("127.0.0.1", port))?
+    .run()
+    .await
 }
 
 #[get("/hash/{hash}")]
-async fn hash(path: web::Path<(String, )>) -> HttpResponse {
+async fn hash(path: web::Path<(String,)>) -> HttpResponse {
     let mail = path.into_inner().0;
     let sha256 = sha256(mail.as_str());
     let md5 = md5(mail.as_str());
@@ -81,7 +87,7 @@ pub struct ImageRequest {
     d: Option<String>,
     default: Option<String>,
     forcedefault: Option<char>,
-    f: Option<char>
+    f: Option<char>,
 }
 
 fn read_size(query: Query<ImageRequest>) -> u16 {
@@ -119,14 +125,21 @@ fn read_forcedefault(query: Query<ImageRequest>) -> bool {
 }
 
 #[get("/{mail}")]
-async fn avatar(path: web::Path<(String, )>, data: web::Data<AppState>, query: Query<ImageRequest>) -> HttpResponse {
+async fn avatar(
+    path: web::Path<(String,)>,
+    data: web::Data<AppState>,
+    query: Query<ImageRequest>,
+) -> HttpResponse {
     let mail_hash = path.into_inner().0;
     let config: Config = data.config.clone();
     let cache_dir = config.images;
     let size: u16 = read_size(query.clone());
     let default: String = read_default(query.clone());
     println!("serving {mail_hash}, size {size}");
-    let mut path = build_path(vec![cache_dir.clone(), size.to_string(), mail_hash.clone()], Some("png".to_string()));
+    let mut path = build_path(
+        vec![cache_dir.clone(), size.to_string(), mail_hash.clone()],
+        Some("png".to_string()),
+    );
     if !path.exists() || read_forcedefault(query) {
         println!("not found {mail_hash}, size {size}, serving {default}");
         match default.as_str() {
@@ -134,10 +147,16 @@ async fn avatar(path: web::Path<(String, )>, data: web::Data<AppState>, query: Q
                 return HttpResponse::NotFound().finish();
             }
             "mm" => {
-                path = build_path(vec![cache_dir.clone(), size.to_string(), "mm".to_string()], Some("png".to_string()));
+                path = build_path(
+                    vec![cache_dir.clone(), size.to_string(), "mm".to_string()],
+                    Some("png".to_string()),
+                );
             }
             _ => {
-                path = build_path(vec![cache_dir.clone(), size.to_string(), default.clone()], Some("png".to_string()));
+                path = build_path(
+                    vec![cache_dir.clone(), size.to_string(), default.clone()],
+                    Some("png".to_string()),
+                );
             }
         }
     }
@@ -148,39 +167,53 @@ async fn avatar(path: web::Path<(String, )>, data: web::Data<AppState>, query: Q
 }
 
 fn resize_default(config: Config) {
-    let path = build_path(vec![config.images.clone(), 512.to_string(), "mm".to_string()], Some("png".to_string()));
-    if !needs_update(&config.raw, path.to_str().unwrap()) {
+    let binding = build_path(
+        vec![config.images.clone(), 512.to_string(), "mm".to_string()],
+        Some("png".to_string()),
+    );
+    let path = binding.as_path();
+    let binding = build_path(
+        vec!["default".to_string(), "mm".to_string()],
+        Some("png".to_string()),
+    );
+    let default = binding.as_path();
+    if !needs_update(default, path) {
         return;
     }
-    let mut handles = vec![];
-    for size in [16, 32, 48, 64, 80, 96, 128, 256, 512].iter() {
+    let sizes: Vec<u32> = vec![16, 32, 48, 64, 80, 96, 128, 256, 512];
+    sizes.par_iter().for_each(|size| {
         let image_path = config.images.clone();
-        create_directory(build_path(vec![image_path.clone(), size.to_string()], None));
-        handles.push(thread::spawn(move || {
-            let path = build_path(vec![image_path.clone(), size.to_string(), "mm".to_string()], Some("png".to_string()));
-            let default_path = build_path(vec!["default".to_string(), "mm.".to_string()], Some("png".to_string()));
-            resize_image(default_path.to_str().unwrap().to_string(), path.to_str().unwrap().to_string(), *size, None).join().unwrap();
-        }));
-    };
-    for handle in handles {
-        handle.join().unwrap();
-    }
+        create_directory(build_path(vec![image_path.clone(), size.to_string()], None).as_path());
+        let binding = build_path(
+            vec![image_path.clone(), size.to_string(), "mm".to_string()],
+            Some("png".to_string()),
+        );
+        let path = binding.as_path();
+        let binding = build_path(
+            vec!["default".to_string(), "mm.".to_string()],
+            Some("png".to_string()),
+        );
+        let default_path = binding.as_path();
+        resize_image(default_path, path, *size, None);
+    });
 }
 
 fn sha256(filename: &str) -> String {
-    let hex_digest = Sha256::digest(filename.as_bytes())
+    Sha256::digest(filename.as_bytes())
         .iter()
-        .map(|byte| format!("{:02x}", byte))
-        .collect::<String>();
-    hex_digest
+        .fold(String::new(), |mut acc, byte| {
+            acc.push_str(&format!("{:02x}", byte));
+            acc
+        })
 }
 
 fn md5(filename: &str) -> String {
-    let hex_digest = Md5::digest(filename.as_bytes())
+    Md5::digest(filename.as_bytes())
         .iter()
-        .map(|byte| format!("{:02x}", byte))
-        .collect::<String>();
-    hex_digest
+        .fold(String::new(), |mut acc, byte| {
+            acc.push_str(&format!("{:02x}", byte));
+            acc
+        })
 }
 
 fn watch_directory(path: String, config: Config) {
@@ -218,17 +251,24 @@ async fn async_watch<P: AsRef<Path>>(path: P, config: Config) -> notify::Result<
     while let Some(res) = rx.next().await {
         match res {
             Ok(event) => match event.kind {
+                notify::EventKind::Modify(ModifyKind::Name(RenameMode::Any)) => {
+                    println!("moved");
+                    if !Path::new(path.as_ref()).exists() {
+                        evacuate_image(&event.paths[0], config.clone());
+                    } else {
+                        processing_image(&event.paths[0], config.clone(), false);
+                    }
+                }
                 notify::EventKind::Create(_) => {
-                    let path = event.paths[0].to_str().unwrap().to_string();
-                    cache_image(path, config.clone());
+                    println!("create");
+                    processing_image(&event.paths[0], config.clone(), false);
                 }
                 notify::EventKind::Modify(ModifyKind::Data(Content)) => {
-                    let path = event.paths[0].to_str().unwrap().to_string();
-                    update_image(path);
+                    println!("modify");
+                    update_image(&event.paths[0], config.clone());
                 }
                 notify::EventKind::Remove(_) => {
-                    let path = event.paths[0].to_str().unwrap().to_string();
-                    evacuate_image(path);
+                    evacuate_image(&event.paths[0], config.clone());
                 }
                 _ => {}
             },
@@ -239,108 +279,146 @@ async fn async_watch<P: AsRef<Path>>(path: P, config: Config) -> notify::Result<
     Ok(())
 }
 
-fn cache_directory(path: String, config: Config) {
-    let paths = fs::read_dir(path).unwrap();
+fn process_directory(directory: &Path, config: Config) {
+    let paths = fs::read_dir(directory).unwrap();
 
-    for path in paths {
-        cache_image(path.unwrap().path().to_str().unwrap().to_string(), config.clone());
+    for path in paths.flatten() {
+        processing_image(&path.path(), config.clone(), false);
     }
 }
 
-fn cache_image(path: String, config: Config) {
-    create_directory(PathBuf::from(&config.images));
-    let filename = get_filename(&path);
-    let image_path = build_path(vec![config.images.to_string(), 512.to_string(), filename], Some("png".to_string()));
-    if !needs_update(&path, image_path.to_str().unwrap()) {
-        return;
-    }
-    println!("caching {}", path);
-
-    handle_image(path, config.clone());
-}
-
-fn handle_image(source: String, config: Config) {
-    let filename = get_filename(&source);
-    let md5_hash = md5(&filename);
-    let sha256 = sha256(&filename);
-
-    let before = Instant::now();
-    let mut handles = vec![];
-    for size in [16, 32, 48, 64, 80, 96, 128, 256, 512].iter() {
-        let size_path = build_path(vec![config.images.clone(), size.to_string()], None);
-        create_directory(size_path.clone());
-        let cache_path = build_path(vec![config.images.clone(), size.to_string(), md5_hash.clone()], Some("png".to_string())).to_str().unwrap().to_string();
-        let link_path = build_path(vec![config.images.clone(), size.to_string(), sha256.clone()], Some("png".to_string())).to_str().unwrap().to_string();
-        if !needs_update(&source, &cache_path) {
+fn processing_image(path: &Path, config: Config, force: bool) {
+    create_directory(Path::new(&config.images));
+    if let Some(filename) = get_filename(path) {
+        let binding = build_path(
+            vec![config.images.to_string(), 512.to_string(), filename],
+            Some("png".to_string()),
+        );
+        let image_path = binding.as_path();
+        if !force && !needs_update(path, image_path) {
             return;
         }
-        println!("resizing {}", cache_path);
-        handles.push(resize_image(source.clone(), cache_path.clone(), *size, Some(link_path)));
+
+        handle_image(path, config.clone());
     }
-    for handle in handles {
-        handle.join().unwrap();
-    }
-    println!("resized {} in {:?}", filename, before.elapsed());
 }
 
-fn resize_image(source: String, destination: String, size: u32, link_path: Option<String>) -> thread::JoinHandle<()> {
-    let img = ImageReader::open(&source).unwrap().decode();
-    println!("resizing {}", source);
-    thread::spawn(move || {
-        img.expect("Cant read image")
-            .resize_to_fill(size, size, image::imageops::FilterType::Lanczos3)
-            .save_with_format(destination.clone(), image::ImageFormat::Png)
-            .unwrap();
-        if let Some(link_path) = link_path {
-            fs::hard_link(destination.clone(), link_path).unwrap();
-        }
-    })
+fn handle_image(source: &Path, config: Config) {
+    if let Some(filename) = get_filename(source) {
+        let md5_hash = md5(&filename);
+        let sha256 = sha256(&filename);
+
+        let before = Instant::now();
+        let sizes: Vec<u32> = vec![16, 32, 48, 64, 80, 96, 128, 256, 512];
+        sizes.par_iter().for_each(|size| {
+            let binding = build_path(vec![config.images.clone(), size.to_string()], None);
+            let size_path = binding.as_path();
+            create_directory(size_path);
+            let binding = build_path(
+                vec![config.images.clone(), size.to_string(), md5_hash.clone()],
+                Some("png".to_string()),
+            );
+
+            let cache_path = binding.as_path();
+            let binding = build_path(
+                vec![config.images.clone(), size.to_string(), sha256.clone()],
+                Some("png".to_string()),
+            );
+
+            let link_path = binding.as_path();
+            if !needs_update(source, cache_path) {
+                return;
+            }
+            println!("resizing {}", cache_path.to_str().unwrap());
+            resize_image(source, cache_path, *size, Some(link_path));
+            println!("resized {} in {:?}", filename, before.elapsed());
+        });
+    }
 }
 
-fn needs_update(path: &str, compare: &str) -> bool {
-    let filename = get_full_filename(path);
+fn resize_image(source: &Path, destination: &Path, size: u32, link_path: Option<&Path>) {
+    let img = ImageReader::open(source).unwrap().decode();
+    println!("resizing {}", source.to_str().unwrap());
+    img.expect("Cant read image")
+        .resize_to_fill(size, size, image::imageops::FilterType::Lanczos3)
+        .save(destination)
+        .unwrap();
+    if let Some(link_path) = link_path {
+        fs::hard_link(destination, link_path).unwrap();
+    }
+}
+
+fn needs_update(raw: &Path, processed: &Path) -> bool {
+    let filename = get_full_filename(raw);
     if filename.starts_with('.') {
         return false;
     }
 
-    if !Path::new(&compare).exists() {
+    if !Path::new(&processed).exists() || !Path::new(raw).exists() {
         return true;
     }
 
-    let raw_metadata = fs::metadata(path).unwrap();
+    let raw_metadata = fs::metadata(raw).unwrap();
     let raw_mtime = FileTime::from_last_modification_time(&raw_metadata);
 
-    if !Path::new(&compare).exists() {
-        return true;
-    }
-    let cache_metadata = fs::metadata(compare).unwrap();
-    let cache_mtime = FileTime::from_last_modification_time(&cache_metadata);
-    raw_mtime > cache_mtime
+    let processed_metadata = fs::metadata(processed).unwrap();
+    let processed_mtime = FileTime::from_last_modification_time(&processed_metadata);
+    raw_mtime > processed_mtime
 }
 
-fn update_image(path: String) {
-    println!("updating {}", path);
-}
-
-fn evacuate_image(path: String) {
-    if get_full_filename(&path).starts_with('.') {
+fn update_image(path: &Path, config: Config) {
+    println!(
+        "updating {} {}",
+        path.to_str().unwrap(),
+        get_full_filename(path).starts_with('.')
+    );
+    if get_full_filename(path).starts_with('.') || !Path::exists(Path::new(&path)) {
         return;
     }
-    println!("evacuating {}", path);
+    println!("updating {}", path.to_str().unwrap());
+    processing_image(path, config, true);
 }
 
-fn get_full_filename(path: &str) -> String {
-    path.split('/').last().unwrap().to_string()
-}
-
-fn get_filename(path: &str) -> String {
-    println!("path: {}", path);
+fn evacuate_image(path: &Path, config: Config) {
     let filename = get_full_filename(path);
-    let extension = Path::new(&path).extension();
-    if let Some(extension) = extension {
-        return filename.replace(format!(".{}", extension.to_str().unwrap()).as_str(), "")
+    if filename.starts_with('.') {
+        return;
     }
-    filename
+    if let Some(extension) = get_extension(&filename) {
+        let md5_hash = md5(&filename);
+        let sha256 = sha256(&filename);
+        let sizes: Vec<u32> = vec![16, 32, 48, 64, 80, 96, 128, 256, 512];
+        sizes.par_iter().for_each(|size| {
+            let size_path = build_path(vec![config.images.clone(), size.to_string()], None);
+            let cache_path = build_path(
+                vec![size_path.to_str().unwrap().to_string(), md5_hash.clone()],
+                Some(extension.to_string()),
+            );
+            let link_path = build_path(
+                vec![size_path.to_str().unwrap().to_string(), sha256.clone()],
+                Some(extension.to_string()),
+            );
+            fs::remove_file(cache_path).unwrap();
+            fs::remove_file(link_path).unwrap();
+        });
+    }
+}
+
+fn get_full_filename(path: &Path) -> String {
+    path.to_str()
+        .unwrap()
+        .split('/')
+        .last()
+        .unwrap()
+        .to_string()
+}
+
+fn get_filename(path: &Path) -> Option<String> {
+    let filename = get_full_filename(path);
+    if let Some(extension) = get_extension(&filename) {
+        return Some(filename.replace(format!(".{extension}").as_str(), ""));
+    }
+    None
 }
 
 fn build_path(parts: Vec<String>, extension: Option<String>) -> PathBuf {
@@ -354,9 +432,17 @@ fn build_path(parts: Vec<String>, extension: Option<String>) -> PathBuf {
     path
 }
 
-fn create_directory(path: PathBuf) {
+fn create_directory(path: &Path) {
     if path.exists() {
         return;
     }
     fs::create_dir_all(path).expect("Could not create directory");
+}
+
+fn get_extension(path: &str) -> Option<String> {
+    let extension = Path::new(&path).extension();
+    if let Some(extension) = extension {
+        return Some(extension.to_str().unwrap().to_string());
+    }
+    None
 }
